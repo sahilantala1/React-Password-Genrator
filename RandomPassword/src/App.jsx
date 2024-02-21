a1cabec8664a2d3b4ae4ab9285d9efd629d599da
Shail Antala <sahilantala16@gmail.com>
2024-02-21 12:37:07 +0530

    copy button edit

--- RandomPassword/src/App.jsx
+++ RandomPassword/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState,useEffect,useRef } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 function App() {
   const [length, setLength] = useState(8)
   const [NumberAllowed, setNumberAllowed] = useState(false)
@@ -20,45 +20,45 @@ function App() {
 
   }, [length, NumberAllowed, CharAllowed, setpassword])
 
-  
-  const PasswordCopy = useCallback(() =>{
-    window.navigator.clipboard.writeText(password)
-  },[password])
-      
-  }
-  useEffect(()=>{
-    passwordGenerator()
-  },[length,NumberAllowed,CharAllowed,passwordGenerator])
 
-  return (
-    <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 pb-2 text-orange-500 bg-slate-500 text-center' >
-        <h1 className='text-white'>Password Genrator</h1>
-        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type='text' value={password} className='outline-none w-full py-1 px-3' placeholder="PassWord" readOnly ref={passwordRef} ></input>
-          <button onClick={PasswordCopy} className='outline-none bg-blue-700 text-white px-3 py-0.5'>Copy</button>
-        </div>
-        <div className='flex text-sm- gap-x-2'>
-          <div className='flex items-center gap-x-1'>
-            <input type='range' min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>{
-              setLength(e.target.value)
-            }}></input>
-            <label>Length:{length}</label>
-          </div>
-          <div className='flex items-center gap-x-1'>
-            <input type='checkbox' defaultChecked={NumberAllowed} id='numberinput' onChange={()=>{
-              setNumberAllowed((prev)=>!prev);
-            }}></input>
-            <label>Numbers</label>
-          </div>
-          <div className='flex items-center gap-x-1'>
-            <input type='checkbox' defaultChecked={CharAllowed} id='charinput' onChange={()=>{
-              setCharAllowed((prev)=>!prev);
-            }}></input>
-            <label>Characters</label>
-          </div>
-        </div>
+  const PasswordCopy = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+
+useEffect(() => {
+  passwordGenerator()
+}, [length, NumberAllowed, CharAllowed, passwordGenerator])
+
+return (
+  <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 pb-2 text-orange-500 bg-slate-500 text-center' >
+    <h1 className='text-white'>Password Genrator</h1>
+    <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+      <input type='text' value={password} className='outline-none w-full py-1 px-3' placeholder="PassWord" readOnly ref={passwordRef} ></input>
+      <button onClick={PasswordCopy} className='outline-none bg-blue-700 text-white px-3 py-0.5'>Copy</button>
+    </div>
+    <div className='flex text-sm- gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input type='range' min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => {
+          setLength(e.target.value)
+        }}></input>
+        <label>Length:{length}</label>
       </div>
-    </>
-  )
+      <div className='flex items-center gap-x-1'>
+        <input type='checkbox' defaultChecked={NumberAllowed} id='numberinput' onChange={() => {
+          setNumberAllowed((prev) => !prev);
+        }}></input>
+        <label>Numbers</label>
+      </div>
+      <div className='flex items-center gap-x-1'>
+        <input type='checkbox' defaultChecked={CharAllowed} id='charinput' onChange={() => {
+          setCharAllowed((prev) => !prev);
+        }}></input>
+        <label>Characters</label>
+      </div>
+    </div>
+  </div>
+)
+}
 export default App
